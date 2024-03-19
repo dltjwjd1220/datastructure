d7dd1c84981aee09fd3a79c70abccd792659ba4d
dltjwjd1220 <129142278+dltjwjd1220@users.noreply.github.com>
2024-03-19 11:47:44 +0900

    Add files via upload

---
+++ 과제3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* int main()
+{
+	int array[2][3][4];
+	int i, j, k, value = 1;
+	for (i = 0; i < 2; i++) { //01 두 번 반복해서 면은 2
+		for (j = 0; j < 3; j++) { //012 세 번 반복해서 행은 3
+			for (k = 0; k < 4; k++) { //0123 네번 반복해서 열은 4
+				array[i][j][k] = value;
+				printf("\narray[%d][%d][%d] = %d", i, j, k, array[i][j][k]);
+				value++;
+			}
+		}
+	}
+	getchar();
+} */
