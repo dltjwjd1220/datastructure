e1528a943e50babc0d124f1dc829d7660e36e69b
dltjwjd1220 <129142278+dltjwjd1220@users.noreply.github.com>
2024-03-19 10:17:54 +0900

    Update 과제1.c

--- 과제1.c
+++ 과제1.c
@@ -2,11 +2,17 @@
 
 int main(void)
 {
-    const char* ptr[4] = { "Korea", "Seoul", "Mapo", "152¹øÁö 2 / 3" };
+    const char* ptr[4] = { "Korea", "Seoul", "Mapo", "152ë²ˆì§€ 2 / 3" };
+    const char* pto[4] = { "Korea", "Seoul", "Jongno", "152ë²ˆì§€ 2 / 3" };
 
-    for (int i = 0; i < 4; i++) {
-        printf("%s\n", ptr[i]);
-    }
+        for (int i = 0; i < 4; i++) {
+            printf("%s\n", ptr[i]);
+        }
+        printf("\n\n");
+
+        for (int j = 0; j < 4; j++) {
+            printf("%s\n", pto[j]);
+        }
 
     return 0;
 }
